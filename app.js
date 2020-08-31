9f94b0054f11a8a06e23ba9b8a8877cf41a174ec
Nolan Terpstra <nbakert@gmail.com>
2020-08-31 18:37:56 -0500

    Saving final function product.

--- app.js
+++ app.js
@@ -9,6 +9,121 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employees = [];
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input"
+                , message: "What is your managers name?"
+                , name: "managerName"
+            },
+            {
+                type: "input"
+                , message: "What is your managers ID?"
+                , name: "managerID"
+            },
+            {
+                type: "input"
+                , message: "What is your managers office number?"
+                , name: "managerNumber"
+            },
+            {
+                type: "input"
+                , message: "What is your managers email?"
+                , name: "managerEmail"
+            },
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerNumber);
+            employees.push(manager);
+            createTeam();
+        });
+};
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input"
+                , message: "What is your engineer's name?"
+                , name: "engineerName"
+            },
+            {
+                type: "input"
+                , message: "What is your engineer's ID?"
+                , name: "engineerId"
+            },
+            {
+                type: "input"
+                , message: "What is your engineer's email?"
+                , name: "engineerEmail"
+            },
+            {
+                type: "input"
+                , message: "What is your engineer's gitHub username?"
+                , name: "engineerUsername"
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerUsername);
+            employees.push(engineer);
+            createTeam();
+        });
+};
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input"
+                , message: "What is your intern's name?"
+                , name: "internName"
+            },
+            {
+                type: "input"
+                , message: "What is your intern's ID?"
+                , name: "internId"
+            },
+            {
+                type: "input"
+                , message: "What is your intern's email?"
+                , name: "internEmail"
+            },
+            {
+                type: "input"
+                , message: "What is the intern's school?"
+                , name: "internSchool"
+            }
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            employees.push(intern);
+            createTeam();
+        });
+};
+function createTeam() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What type of employee would you like to add next?",
+                choices: ["Engineer", "Intern", "I have no more employees to add"],
+                name: "employeeType"
+            }
+        ]).then((answer) => {
+            if (answer.employeeType === "Engineer") {
+                addEngineer();
+            }
+            else if (answer.employeeType === "Intern") {
+                addIntern();
+            }
+            else {
+                fs.writeFile(outputPath, render(employees), function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log("Success, your team has been created.")
+                })
+            }
+        })
+}
+addManager();
 
 
 // Write code to use inquirer to gather information about the development team members,
